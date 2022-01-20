dc72c256861f8e0d5538647608feacabd1e6d664
zyuhong <10756009@gmail.com>
2022-01-20 14:23:36 +0800

    feat(auth): add login register page

--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from utils.auth_views import register
 
+
+login_params = {
+    'template_name': 'users/login.html',
+    'redirect_authenticated_user': True,
+}
 urlpatterns = [
+    path('', lambda request: redirect('coffees:index'), name='root'),
     path('coffees/', include('coffees.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(**login_params), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', register, name='register'),
 ]
 
